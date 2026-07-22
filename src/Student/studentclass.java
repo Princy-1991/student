27676ed07db4cdf60869337475f0611166f575a6
Harleynoah@2024 <princygeorgepush@gmail.com>
2026-07-22 10:47:22 +0530

    git practice

--- src/Student/studentclass.java
+++ src/Student/studentclass.java
@@ -4,6 +4,7 @@ public class studentclass {
 	public static void main(String [] args) {
 		System.out.println("Hello git");
 		System.out.println("Welcome to Git hub");
+		System.out.println("Git Practice day");
 	}
 
 }
